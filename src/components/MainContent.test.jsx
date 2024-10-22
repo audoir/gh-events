fc87063f0657793c7e22dcc52d24ea0e2bb7c16a
audoir <wayne@audoir.com>
2024-10-22 11:57:19 -0700

    added comments [skip ci]

--- src/components/MainContent.test.jsx
+++ src/components/MainContent.test.jsx
@@ -5,6 +5,7 @@ import userEvent from '@testing-library/user-event';
 import MainContent from './MainContent';
 
 // My tests
+// Another one
 
 describe('MainContent', () => {
   it('should render a button', () => {
